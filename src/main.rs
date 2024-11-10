283b8e0867a1307a61e35f9b47888c57c536fd8e
Igor Jakus <igorjakus@protonmail.com>
2024-11-10 18:53:04 +0100

    feat: Add transposition table

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time::Instant;
+use std::collections::HashMap;
 use chess::{Board, BoardStatus, Color, MoveGen, Piece};
 
 const PIECE_VALUES: [(Piece, i32); 6] = [
@@ -10,7 +11,12 @@ const PIECE_VALUES: [(Piece, i32); 6] = [
     (Piece::King, 1000),
 ];
 
-fn evaluation(board: &Board) -> i32 {
+fn evaluation(board: &Board, transposition_table: &mut HashMap<u64, i32>) -> i32 {
+    let board_hash = board.get_hash();
+    if let Some(&cached_eval) = transposition_table.get(&board_hash) {
+        return cached_eval;
+    }
+
     let mut score = 0;
 
     for (piece, value) in PIECE_VALUES.iter() {
@@ -21,13 +27,17 @@ fn evaluation(board: &Board) -> i32 {
         score -= black_pieces.popcnt() as i32 * value;
     }
 
+    transposition_table.insert(board_hash, score);
     score
 }
 
 
-fn minmax(board: &Board, depth: i32, alpha: i32, beta: i32, maximize: bool) -> i32 {
+fn minmax(
+    board: &Board, depth: i32, alpha: i32, beta: i32, maximize: bool, 
+    transposition_table: &mut HashMap<u64, i32>
+) -> i32 {
     if depth == 0 || board.status() != BoardStatus::Ongoing {
-        return evaluation(board)
+        return evaluation(board, transposition_table)
     }
     
     let mut alpha = alpha;
@@ -38,7 +48,7 @@ fn minmax(board: &Board, depth: i32, alpha: i32, beta: i32, maximize: bool) -> i
     if maximize {
         for m in MoveGen::new_legal(&board) {
             let neighbour = board.make_move_new(m);
-            eval = eval.max(minmax(&neighbour, depth - 1, alpha, beta, false));
+            eval = eval.max(minmax(&neighbour, depth - 1, alpha, beta, false, transposition_table));
             alpha = alpha.max(eval);
 
             if beta <= alpha {
@@ -49,7 +59,7 @@ fn minmax(board: &Board, depth: i32, alpha: i32, beta: i32, maximize: bool) -> i
     } else {
         for m in MoveGen::new_legal(&board) {
             let neighbour = board.make_move_new(m);
-            eval = eval.min(minmax(&neighbour, depth - 1, alpha, beta, true));
+            eval = eval.min(minmax(&neighbour, depth - 1, alpha, beta, true, transposition_table));
             beta = beta.min(eval);
             
             if beta <= alpha {
@@ -58,15 +68,17 @@ fn minmax(board: &Board, depth: i32, alpha: i32, beta: i32, maximize: bool) -> i
         }
     }
 
+    transposition_table.insert(board.get_hash(), eval);
     eval
 }
 
 fn main() {
     let board = Board::default();
+    let mut transposition_table = HashMap::new();
 
     let start = Instant::now();
     
-    let eval = minmax(&board, 8, i32::MIN, i32::MAX, true);
+    let eval = minmax(&board, 7, i32::MIN, i32::MAX, true, &mut transposition_table);
     println!("Minmax result: {}", eval);
     
     let duration = start.elapsed();
